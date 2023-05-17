12641d1a90356f58acfde9203aa45472ecb47b51
Waldis Henriquez <waldis.henriquez@solvex.com.do>
2023-05-17 13:20:46 -0400

    feat: added single pokemon model

--- Pokedex-Frontend/src/app/models/single-pokemon.ts
+++ Pokedex-Frontend/src/app/models/single-pokemon.ts
@@ -1,2 +1,22 @@
 export interface SinglePokemon {
+  id: number;
+  name: string;
+  images?: PokemonImages | null;
+  types?: Types[] | null;
+  isFavorite: boolean;
+}
+
+export interface PokemonImages {
+  front_default: string;
+  back_default: string;
+}
+
+export interface Types {
+  slot: number;
+  type: TypeDetail | null;
+}
+
+export interface TypeDetail {
+  name: string;
+  url: string;
 }
