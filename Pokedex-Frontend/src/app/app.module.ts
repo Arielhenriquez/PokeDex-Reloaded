5e7947829e3199227dc1df00002ad44f50c380ca
Waldis Henriquez <waldis.henriquez@solvex.com.do>
2023-05-21 22:50:59 -0400

    feat: added ng prime styles module

--- Pokedex-Frontend/src/app/app.module.ts
+++ Pokedex-Frontend/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPokemonsComponent } from './components/list-pokemons/list-pokemons.component';
 import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 import { PokemonService } from './services/pokemon.service';
+import { NgPrimeModule } from './modules/ng-prime/ng-prime.module';
 
 @NgModule({
   declarations: [AppComponent, ListPokemonsComponent, PokemonDetailsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgPrimeModule,
+  ],
   providers: [PokemonService],
   bootstrap: [AppComponent],
 })
